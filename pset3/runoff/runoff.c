ffe5413572e4d540f8a8be5351e2b80fc9b3374b
yanase-kenta0505 <ynsknt0505@gmail.com>
2023-10-08 20:38:45 +0900

    eliminate関数を実装

--- pset3/runoff/runoff.c
+++ pset3/runoff/runoff.c
@@ -224,6 +224,13 @@ bool is_tie(int min)
 // Eliminate the candidate (or candidates) in last place
 void eliminate(int min)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (!candidates[i].eliminated && candidates[i].votes == min)
+        {
+            candidates[i].eliminated = true;
+        }
+    }
+
     return;
 }
