715fc6076ceaed57a15fc6d4abb3f8aae0ee428d
yanase-kenta0505 <ynsknt0505@gmail.com>
2023-10-08 19:33:30 +0900

    find_min関数を実装

--- pset3/runoff/runoff.c
+++ pset3/runoff/runoff.c
@@ -185,15 +185,25 @@ bool print_winner(void)
         return false;
     }
 
-    // TODO
     return false;
 }
 
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    // TODO
-    return 0;
+    int min_votes = INT_MAX; 
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+
+        if (!candidates[i].eliminated &&  candidates[i].votes < min_votes)
+        {
+            min_votes = candidates[i].votes;
+        }
+    }
+    
+
+    return min_votes;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
