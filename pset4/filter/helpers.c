d68a3019c869163dbf07345c1a1ca26f98143f9a
yanase-kenta0505 <ynsknt0505@gmail.com>
2023-10-09 20:49:45 +0900

    sepia関数を実装

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -1,5 +1,21 @@
 #include "helpers.h"
 
+int integer_control(int value)
+{
+    if (value < 0)
+    {
+        return 0;
+    }
+    else if (value > 255)
+    {
+        return 255;
+    }
+    else
+    {
+        return value;
+    }
+}
+
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
@@ -23,6 +39,20 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int sepiaRed = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
+            int sepiaGreen = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue;
+            int sepiaBlue = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue;
+
+            image[i][j].rgbtBlue = integer_control(sepiaBlue);
+            image[i][j].rgbtGreen = integer_control(sepiaGreen);
+            image[i][j].rgbtRed = integer_control(sepiaRed);
+        }
+    }
+
     return;
 }
 
