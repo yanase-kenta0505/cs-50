19eb2bf9dd8800e764af268168a0810c5ecde298
yanase-kenta0505 <ynsknt0505@gmail.com>
2023-10-09 18:55:21 +0900

    grayscaleを実装

---
+++ pset4/filter/helpers.c
@@ -0,0 +1,39 @@
+#include "helpers.h"
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            RGBTRIPLE origin_image = image[i][j];
+
+            int average_color = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
+
+            image[i][j].rgbtBlue = average_color;
+            image[i][j].rgbtGreen = average_color;
+            image[i][j].rgbtRed = average_color;
+        }
+    }
+
+    return;
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
