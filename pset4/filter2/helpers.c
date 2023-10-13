94ec8c6a45f2d0ab46e041a6d208be71636b2e47
yanase-kenta0505 <ynsknt0505@gmail.com>
2023-10-13 18:28:03 +0900

    スタイルの修正

--- pset4/filter2/helpers.c
+++ pset4/filter2/helpers.c
@@ -33,7 +33,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width / 2; j++)
         {
-            swap(&image[i][j], &image[i][width - 1 -j]);
+            swap(&image[i][j], &image[i][width - 1 - j]);
         }
     }
 
